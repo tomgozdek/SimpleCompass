7b8bc2387b87a25922b0997b57deaf3db37b84b3
tomgozdek <tomgozdek@gmail.com>
2020-07-02 23:53:51 +0200

    Add low pass filter to reduce noise impact on compass needle readings

--- app/src/main/java/com/tomgozdek/simplecompass/compass/OrientationObserver.kt
+++ app/src/main/java/com/tomgozdek/simplecompass/compass/OrientationObserver.kt
@@ -8,6 +8,11 @@ import android.hardware.SensorManager
 import androidx.lifecycle.LiveData
 
 class OrientationObserver(context: Context) : LiveData<OrientationDataModel>(), SensorEventListener {
+
+    companion object {
+        const val FILTER_FACTOR = 0.20F
+    }
+
     private val sensorManager =
         context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
@@ -25,13 +30,24 @@ class OrientationObserver(context: Context) : LiveData<OrientationDataModel>(), 
     override fun onSensorChanged(sensorEvent: SensorEvent?) {
 
         when (sensorEvent?.sensor?.type) {
-            Sensor.TYPE_MAGNETIC_FIELD -> magnetometerData = sensorEvent.values
-            Sensor.TYPE_ACCELEROMETER -> accelerometerData = sensorEvent.values
+            Sensor.TYPE_MAGNETIC_FIELD -> magnetometerData = filter(sensorEvent.values!!, magnetometerData)
+            Sensor.TYPE_ACCELEROMETER -> accelerometerData = filter(sensorEvent.values!!, accelerometerData)
             else -> return
         }
 
         value = calculateCompassData()
 
+    }
+
+    /**
+     * Reduces noise present in sensor readings
+     */
+    private fun filter(values: FloatArray, output: FloatArray): FloatArray {
+        for((index, value) in values.withIndex()){
+            output[index] = output[index] + FILTER_FACTOR * (value - output[index])
+        }
+
+        return output
     }
 
     private fun calculateCompassData() : OrientationDataModel {
